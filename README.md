24ec2d3e1d109836f8fccdfd51f25313749b94f8
losthope <arsen.melnuchyk@gmail.com>
2024-07-17 12:58:36 +0300

    implemented selecting from the list method

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ Here is the steps to reproduce:
 2. source .env/bin/activate
 3. pip install -r requirements.txt
 4. pytest
+
+## Additional features and requirements for the project
+
+1. Be able to read firstnames and surnames from the text file
+2. Create variables and function names understand for the people who don't even know python
--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 import random
 
-def main():
-    pass
+def select_name_from_file(filename: str) -> str:
+    with open(filename, "r+") as file:
+        names: list = file.read()
 
+    selected_name = random.choice(names)
+    return selected_name
+
+
+def select_name_from_list(names: list) -> str:
+    selected_name = random.choice(names)
+    return selected_name
+
+
+def main() -> None:
+    firstnames = ["Cucumber", "Toes"]
+    surnames = ["Cocka", "Buka"]
+
+    name = select_name_from_list(firstnames)
+    surname = select_name_from_list(surnames)
+
+    print(f"{name} {surname}")
+    
 
 if __name__ == "__main__":
     main()
